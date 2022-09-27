dfae1213a02e1fc1672c4526cb8a20225ea9cb1d
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-09-27 14:19:13 +1000

    js API functionality edited

--- script.js
+++ script.js
@@ -5,16 +5,17 @@
 // MY KEY: d6e0ea5fdd70eb0c05228e0f41cc3c81
 
 var WeatherAPIKey = "d6e0ea5fdd70eb0c05228e0f41cc3c81";
-var url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WeatherAPIKey}`;
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + WeatherAPIKey;
-//var cityEl = document.getElementById("search-input");
-var searchFormEl = document.getElementById('#search-form');
-var nameEl = document.getElementById(".city-name");
-var todayweatherEl = document.getElementById(".todays-date");
-var todayweatherEl = document.getElementById(".todays-weather");
-var currentTempEl = document.getElementById("#temperature");
-var currentHumidityEl = document.getElementById("#humidity");
-var currentWindEl = document.getElementById("#wind");
+//var url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WeatherAPIKey}`;
+//var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + WeatherAPIKey;
+var buttonEl = document.getElementById("search-button");
+var cityEl = document.getElementById("search-input");
+var searchFormEl = document.getElementById('search-form');
+var nameEl = document.getElementById("city-name");
+var todayweatherEl = document.getElementById("todays-date");
+var todayweatherEl = document.getElementById("todays-weather");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindEl = document.getElementById("wind");
 
 
 // when user enters their city location in #search-input overwritng the 'placeholder text'
@@ -24,28 +25,37 @@ var currentWindEl = document.getElementById("#wind");
 //below, the CARD SUBTITLE includes the city's .temperature .wind .humidty
 
 
-function getWeather(cityName) {
+function getWeather() {
+    var cityName = cityEl.value;
+    console.log(cityName)
     //Current weather get request from open weather api
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + WeatherAPIKey;
-    axios.get(queryURL)
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + WeatherAPIKey;
+    console.log(queryURL)
+    fetch(queryURL)
         .then(function (response) {
 
-            todayweatherEl.classList.remove("d-none");
+            return response.json()
+          // console.log(response);
+           //debugger;
+            //todayweatherEl.classList.remove("d-none");
 
              // Parse response to display current weather
-             var currentDate = new Date(response.data.dt * 1000);
-             var day = currentDate.getDate();
-             var month = currentDate.getMonth() + 1;
-             var year = currentDate.getFullYear();
-             nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
-             currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
-             currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-        });
+            // var currentDate = new Date(response.data.dt * 1000);
+            // var day = currentDate.getDate();
+             //var month = currentDate.getMonth() + 1;
+             //var year = currentDate.getFullYear();
+             //nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+             //currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+             //currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        }).then(function (response){
+console.log(response)
+debugger
+        })
     }
 
     // event listener should go here to bring elements to Main Card? OR should the above be a FETCH request?
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+buttonEl.addEventListener('click', getWeather);
 
 getWeather();
     // below the MAIN card, each of the CARDs includes the city's .date .temperature .wind .humidty
